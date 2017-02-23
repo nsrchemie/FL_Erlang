f21d588e80bc15ad65ed0a50c6955fff675942a4
nsrchemie <nsrchemie@gmail.com>
2017-02-23 14:36:20 -0500

    refactored hypotenuse fn

--- second.erl
+++ second.erl
@@ -2,8 +2,7 @@
 -export([hypo/2, triper/2, triarea/2]).
 
 hypo(A,B) ->
-Csq = first:square(A) + first:square(B),
-math:sqrt(Csq).
+math:sqrt(first:square(A) + first:square(B)).
 
 triper(A,B) ->
 A+B+ hypo(A,B).
