f046fd13372d9b9bd76ad76857a107454beeb206
nsrchemie <nsrchemie@gmail.com>
2017-02-24 13:37:51 -0500

    added simple factorial fn

--- second.erl
+++ second.erl
@@ -1,5 +1,5 @@
 -module(second).
--export([hypo/2, triper/2, triarea/2]).
+-export([hypo/2, triper/2, triarea/2, fact/1]).
 
 hypo(A,B) ->
 math:sqrt(first:square(A) + first:square(B)).
@@ -10,4 +10,7 @@ A+B+ hypo(A,B).
 triarea(A,B) ->
 (A*B)/2.
 
-
+fact(1) ->
+1;
+fact(A) ->
+A * fact(A-1).
