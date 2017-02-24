51b2529546b2ea4f263c3768ecfc883049382055
nsrchemie <nsrchemie@gmail.com>
2017-02-24 16:39:31 -0500

    list head and tail(rest) fn

--- intro2/wlis.erl
+++ intro2/wlis.erl
@@ -1,8 +1,14 @@
 -module(wlis).
--export([add_end_list/2, sub_list/2]).
+-export([add_end_list/2, sub_list/2, first_elist/1, rest_list/1]).
 
 add_end_list(L, P) ->
 L ++ P.
 
 sub_list(L, P) ->
 L -- P.
+
+first_elist(L) ->
+hd(L).
+
+rest_list(L) ->
+tl(L).
