108d36264aba8a21a171375a50a884fad5e9930e
nsrchemie <nsrchemie@gmail.com>
2017-02-24 15:34:25 -0500

    changing tuple elements

--- intro2/wtup.erl
+++ intro2/wtup.erl
@@ -1,5 +1,5 @@
 -module(wtup).
--export([creatup/3,firstup/1]).
+-export([creatup/3,firstup/1, changethird/2]).
 
 creatup(A,B,C) ->
 % Can mix data types in tuple
@@ -7,3 +7,6 @@ TUP = {A, B, C}.
 
 firstup(T) ->
 element(1, T).
+
+changethird(T, V) ->
+setelement(3,T,V).
