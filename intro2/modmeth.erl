59c6becc9b2140f6cd4f73f59a68eafe08db4354
nsrchemie <nsrchemie@gmail.com>
2017-02-24 15:50:30 -0500

    using output fwrite

--- intro2/modmeth.erl
+++ intro2/modmeth.erl
@@ -1,8 +1,11 @@
 -module(modmeth).
--export([lisort/1, prname/1]).
+-export([lisort/1, prname/1, greetio/0]).
 
 lisort(A) ->
 lists:sort(A).
 
 prname(Name) ->
 string:concat("Hello ", Name).
+
+greetio() ->
+io:fwrite("Hello!\n").
