60791b8e4b4d263b80f162704ed6906ef5504257
nsrchemie <nsrchemie@gmail.com>
2017-02-25 16:28:08 -0500

    tuple comprehension with pattern matching comment

--- intro2/wtup.erl
+++ intro2/wtup.erl
@@ -15,3 +15,10 @@ setelement(3,T,V).
 % Pl = [{"Belladonna", 10}, {"Bacopa", 3}, {"Psuedopanax",40}]
 % e.g [{Plant, Alkaloid_conc_mg} || {Name, Alkaloid_conc_mg} <- Pl, Alkaloid_conc_mg >= 10]
 % -> [{"Psuedopanax",40}]
+% 
+% similar but using symbols instead of strings for filtering
+% Plants = [{cardamine,brassicaceae}, {nepeta,lamiaceae},{melissa, lamiaceae}, {alliaria,brassicaceae},{capsella,brassicaceae}].
+% 
+% Mustards = [G || {G, brassicaceae} <- Plants].
+% [cardamine,alliaria,capsella]
+
