009182bf450d92194f6ef90ed651e259d934c897
nsrchemie <nsrchemie@gmail.com>
2017-03-02 01:21:01 -0500

    added two dimensional line slice fn

--- wk0/intro3/recurs.erl
+++ wk0/intro3/recurs.erl
@@ -1,5 +1,5 @@
 -module(recurs).
--export([fac/1,fib/1]).
+-export([fac/1,fib/1, tds/1]).
 
 fac(0) ->
 1;
@@ -9,3 +9,6 @@ N*fac(N-1).
 fib(0) -> 0;
 fib(1) -> 1;
 fib(N) -> fib(N-1)+fib(N-2).
+
+tds(0) -> 1;
+tds(N) -> N+tds(N-1).
