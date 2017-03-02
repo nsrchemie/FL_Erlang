d79b397e5b3a6fe2ed0dbdd328d862113f5e9ba6
nsrchemie <nsrchemie@gmail.com>
2017-03-02 16:46:11 -0500

    tail recursive variant of fibonacci

--- wk0/intro3/recurs2.erl
+++ wk0/intro3/recurs2.erl
@@ -1,12 +1,18 @@
 -module(recurs2).
--export([fac/1,loop/1]).
+-export([fib/3,fib/1,fac/1,loop/1]).
+
+fib(0,P,_C)->
+  P;
+fib(N,P,C)->
+  fib(N-1,C,P+C).
+fib(N)->
+  fib(N,0,1).
+
 
 fac(N) -> 
   fac(N,1).
-
 fac(0,P) -> 
   P;
-
 fac(N,P) when N>0 ->
   fac(N-1, P*N).
 
