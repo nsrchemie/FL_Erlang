cd594a24fa470183aaedf6e83d8f4369ef45079f
nsrchemie <nsrchemie@gmail.com>
2017-03-02 14:27:02 -0500

    simple num dec/pr\n loop

--- wk0/intro3/recurs2.erl
+++ wk0/intro3/recurs2.erl
@@ -1,5 +1,5 @@
 -module(recurs2).
--export([fac/1]).
+-export([fac/1,loop/1]).
 
 fac(N) -> 
   fac(N,1).
@@ -9,3 +9,11 @@ fac(0,P) ->
 
 fac(N,P) when N>0 ->
   fac(N-1, P*N).
+
+
+loop(N) when N> 0 ->
+  io:format("~p~n",[N]),
+  loop(N-1);
+
+loop(0) ->
+  io:format("bye~n").
