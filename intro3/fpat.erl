d68f2f4f45cbdee5ebbda65286b3c0cb9e83d702
nsrchemie <nsrchemie@gmail.com>
2017-03-01 21:26:46 -0500

    arg equivalence fn

--- intro3/fpat.erl
+++ intro3/fpat.erl
@@ -1,5 +1,16 @@
 -module(fpat).
--export([maxThree/3]).
+-export([maxThree/3, numEqual/3]).
 
 maxThree(A,B,C) ->
 max(max(A,B),C).
+
+numEqual(A,A,A) ->
+3;
+numEqual(A,A,_) ->
+2;
+numEqual(A,_,A) ->
+2;
+numEqual(_,A,A) ->
+2;
+numEqual(_,_,_) ->
+0. 
