3709756cf2e9b44df8fa37db149fb6ce8a209c3f
nsrchemie <nsrchemie@gmail.com>
2017-03-01 22:56:21 -0500

    binary example of hardtyped fn

--- intro3/fpat.erl
+++ intro3/fpat.erl
@@ -1,5 +1,5 @@
 -module(fpat).
--export([maxThree/3, numEqual/3]).
+-export([xOr1/2, xOr2/2, xOr3/2, xOrb/2, maxThree/3, numEqual/3]).
 
 xOr1(A,B) ->
 A =/= B.
@@ -10,6 +10,10 @@ not(A==B).
 xOr3(A,B) ->
 (not(A) and B) or (A and not(B)).
 
+xOrb(true,X) ->
+not(X);
+xOrb(false, X) ->
+X.
 
 maxThree(A,B,C) ->
 max(max(A,B),C).
