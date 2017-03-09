c7e5b7a68a41b0e43098ae47f33a711ab6f06213
nsrchemie <nsrchemie@gmail.com>
2017-03-08 20:46:21 -0500

    triple all elements of list

--- wk1/list_recur.erl
+++ wk1/list_recur.erl
@@ -1,8 +1,12 @@
 -module(list_recur).
--export([sum/1]).
+-export([sum/1, triple/1]).
 
 %Sum elements of a list
 sum([])-> 0;
 
 sum([H|T]) ->
 H + sum(T).
+
+%Multiply elements of list by three
+triple([])-> [];
+triple([H|T])->[H*2|triple(T)].
